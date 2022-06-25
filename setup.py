6c243b60c0378239600c84e4f1d57dbb6c1adc15
Eyosiyas Bereketab <mreyosivip@gmail.com>
2022-06-25 20:51:45 +0300

    add license and setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+import setuptools
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+setuptools.setup(
+    name='Upwork Scraper',
+    version='1.0.0',
+    author='Eyosiyas Bereketab',
+    author_email='contact@deveyosiyas.com',
+    description='Upwork talent scraper',
+    long_description='A lightweight Python package to scrape upwork.com for talent profiles',
+    long_description_content_type='text/markdown',
+    url='https://github.com/devEyosiyas/upwork_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
